fe63151b75c7cfd71bc06340aa71e0ce9ce0a388
Nikunj Gohil <nikunj.srashtasoft@gmail.com>
2022-07-08 11:39:05 +0530

    heroku

--- backend/urls.py
+++ backend/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path , include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("app.urls")),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
